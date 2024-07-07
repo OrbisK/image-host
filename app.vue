e177aef4165951bc8f92341f39b9a112e821f9bc
Robin Kehl <robin.kehl@singular-it.de>
2024-07-07 17:08:40 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtPage></NuxtPage>
+</template>
+
+
+<style>
+html,
+body,
+#__nuxt {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow: visible;
+}
+
+html.dark {
+  background: #222;
+  color: white;
+}
+</style>
---
+++ pages/[path].vue
@@ -0,0 +1,8 @@
+<template>
+  <img :src="`${route.params.path}.png`" style="width: 100%; max-height: 99vh" />
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+</script>
